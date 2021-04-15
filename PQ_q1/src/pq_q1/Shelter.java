50997df118f7a5dafee937d1aa0202c9f40f07f6
lee <lee.berte@pm.me>
2021-04-15 04:25:59 +0100

    working updates

--- PQ_q1/src/pq_q1/Shelter.java
+++ PQ_q1/src/pq_q1/Shelter.java
@@ -1,21 +1,34 @@
 package pq_q1;
 import java.util.*; 
 
+
 // Complete the class Shelter here
 
 public class Shelter
 {
    //instance variables
    private int currentWeek;
+   private  List<Animal> animals;
 
+
+
+   
    /**
     * Constructor for objects of class Shelter
     */
    public Shelter()
    {
       //complete the constructor
+      animals = new ArrayList<>();
+      currentWeek = 0;
+
    }
    
+   //getter for animals
+   public List<Animals> getAnimals(){
+      return animals;
+   }
+
    public int getCurrentWeek()
    {
        return currentWeek;
@@ -24,6 +37,9 @@ public class Shelter
    public void addAnimal(String aKind, String aName, String aDescription)
    {
        //complete this method
+       getAnimals().add(new Animal(getCurrentWeek(), aKind, aName, aDescription));
+       
+
    }
    
     /**
@@ -40,5 +56,51 @@ public class Shelter
       addAnimal("tortoise","Speedy","Horsefield");
       currentWeek = 3;
    }
+
+
+   // Returns boolean if the animal has been at the shelter over 4 weeks
+
+   public boolean inTheLastMonth(Animal anAnimal){
+      int calWeeks = 52;
+      int currentWeekVal = currentWeek;
+      int animalWeekVal = anAnimal.getWeek();
+      int weeksAtShelter;
+      if (currentWeekVal < animalWeekVal){
+         weeksAtShelter = calWeeks - (animalWeekVal - currentWeekVal);
+      }
+      else{
+         weeksAtShelter = currentWeekVal - animalWeekVal;
+      }
+      return weeksAtShelter <= 4;
+
+   }
+
+   //returns data of animal brought in within the last month
+   public void showRecentAnimals(){
+   for (Animal anAnimal : animals){
+         if(inTheLastMonth(anAnimal)){
+            System.out.println(anAnimal.getKind()
+                     .concat(" ").concat(anAnimal.getName())
+                     .concat(":").concat(anAnimal.getDescription()));
+         }
+         else{
+            System.out.println("No recent animals");
+         }
+      }
+   }
+   public boolean homed(int aWeek, String aKind, String aName){
+      boolean matched = false;
+      for (Animal theAnimal : animals) {
+         if((theAnimal.getWeek() == aWeek) 
+             && (theAnimal.getKind().equals(aKind))
+             && (theAnimal.getName().equals(aName))){
+                matched = true;
+          }
+          if (matched){
+             getAnimals().remove(theAnimal);
+          }
+       }
+      return matched;
+    }
 }
 
